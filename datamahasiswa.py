a27317a2ea76d0a815a8eef014f0a3d1f9cc0c0c
rehan-prass <55973421+rehan-prass@users.noreply.github.com>
2019-10-04 15:35:08 +0700

    coba data mahasiswa lain

---
+++ datamahasiswa.py
@@ -0,0 +1,23 @@
+pilihan = int (input("Masukkan NPM : "))
+if pilihan == 54411579 :
+    print ("Nama : Mohammed salah")
+    print ("NPM : 54411579")
+    print ("IP : 3.9")
+elif pilihan == 12345678 :
+    print ("Nama : raihan")
+    print ("NPM : 12345678")
+    print ("IP : 3.5")
+elif pilihan == 54321234 :
+    print ("Nama : micun")
+    print ("NPM : 54321234")
+    print ("IP : 3.5")
+elif pilihan == 55432122 :
+    print ("Nama : Syammm")
+    print ("NPM : 5432122")
+    print ("IP : 3.5")
+elif pilihan == 23098713 :
+    print("Nama : akbar")
+    print("Npm : 23098713")
+    print("IP : 4.0")
+else:
+    print ("NPM tidak ada")
