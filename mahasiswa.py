e0ce9e074c61eea93a4c86691121c0e869271b14
rehan-prass <55973421+rehan-prass@users.noreply.github.com>
2019-10-08 16:50:09 +0700

    udah berhasil kakak

--- mahasiswa.py
+++ mahasiswa.py
@@ -13,7 +13,7 @@ while True:
         print
         nama.pop(i)
         npm.pop(i)
-        continue
+        break
     kls.append(input('Kelas Anda : '))
     if len(kls[i])!=5:
         print ('Kelas Salah')
@@ -37,44 +37,6 @@ while True:
         print ('Kelas Salah')
         print
      
-nama=[]
-npm=[]
-kls=[]
-jur=[]
-ang=[]
-while True:
-    print ('Masukkan data ke -  ',i+1)
-    nama.append(input('Nama Anda : '))
-    npm.append(input('NPM Anda : '))
-    if len(npm[i])!=8:
-        print ('NPM Salah')
-        print
-        nama.pop(i)
-        npm.pop(i)
-        continue
-    kls.append(input('Kelas Anda : '))
-    if len(kls[i])!=5:
-        print ('Kelas Salah')
-        print
-        nama.pop(i)
-        npm.pop(i)
-        kls.pop(i)
-        continue
-    jur.append(kls[i][1:3])
-    ang.append(npm[i][3:5])
-    if jur[i]==('IA'):
-        jur[i]=('Teknik Informatika')
-        a+=1
-    elif jur[i]=='IB':
-        jur[i]='Teknik Industri'
-        b+=1
-    elif jur[i]=='IC':
-        jur[i]='Teknik Mesin'
-        c+=1
-    else:
-        print ('Kelas Salah')
-        print
-        continue
     lagi=''
     while lagi!='y' and lagi!='t':
         lagi=input('INPUT LAGI [Y/T] : ')
